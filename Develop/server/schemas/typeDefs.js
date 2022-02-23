b075903b9667f645a01c0b8458a940e80f435bee
lilslash <maxwittner01@gmail.com>
2022-02-22 21:54:01 -0700

    book search

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server-express');
+
+
+const typeDefs = gql `
+type book {
+    authors: [String]
+    description: String
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+type user {
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int-
+    savedBooks: [book]
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+input SavedBookInput {
+    authors: [String]
+    title: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: SavedBookInput): User
+    removeBook(bookId: String!): User
+}
+`
+
+
+module.exports = typeDefs;
